796b6ad154fc95829cb9374c4cd9a8944faadb53
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-08-15 10:21:17 +0300

    Merge Bootstrap 3 and 4 components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Form from './Form'
-import HasError from './components/HasError.vue'
-import HasError4 from './components/HasError4.vue'
-import AlertError from './components/Error.vue'
-import AlertErrors from './components/Errors.vue'
-import AlertSuccess from './components/Success.vue'
+import HasError from './components/HasError'
+import AlertError from './components/Error'
+import AlertErrors from './components/Errors'
+import AlertSuccess from './components/Success'
 
 export {
   Form,
   HasError,
-  HasError4,
   AlertError,
   AlertErrors,
   AlertSuccess,
