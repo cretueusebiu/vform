7d5b3b9ea561207db771eadba0ed1f4d7f37d80f
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-09-01 09:23:34 +0300

    Lint

--- src/components/AlertSuccess.vue
+++ src/components/AlertSuccess.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="form.successful" class="alert alert-success alert-dismissible" role="alert">
-    <button v-if="dismissible" @click="dismiss" type="button" class="close" aria-label="Close">
+    <button v-if="dismissible" type="button" class="close" aria-label="Close" @click="dismiss">
       <span aria-hidden="true">&times;</span>
     </button>
 
