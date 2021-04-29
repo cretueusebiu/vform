727f31866a3709e09d3883b2179c893cb6ae36c5
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2021-04-29 16:18:05 +0300

    Refactor

--- site/postcss-nested-import.js
+++ site/postcss-nested-import.js
@@ -1,43 +1,38 @@
 // https://github.com/eriklharper/postcss-nested-import
-const fs = require('fs')
-const postcss = require('postcss')
+const { readFileSync } = require('fs')
 
 function parseImportPath (path) {
   const matches = path.trim().match(/^['"](.+?)['"](.*)/)
-  // const modes = matches[2].trim().split(' ');
-  const fragment = matches[1]
-  return fragment
+  return matches ? matches[1] : null
 }
 
-function readFile (file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf-8', (err, contents) => {
-      if (err) {
-        return reject(err)
-      }
-      return resolve(contents)
-    })
-  })
+function readFile (path) {
+  try {
+    return readFileSync(path, { encoding: 'utf-8' })
+  } catch (e) {
+    try {
+      return readFileSync(`node_modules/${path}`, { encoding: 'utf-8' })
+    } catch (e) {}
+
+    throw e
+  }
 }
 
 module.exports = () => {
   return {
     postcssPlugin: 'postcss-nested-import',
     Once (root) {
-      return new Promise((resolve) => {
-        let prom = Promise.resolve()
-        root.walkAtRules('nested-import', (importAtRule) => {
-          if (importAtRule.params) {
-            const path = parseImportPath(importAtRule.params)
-            if (path === null) {
-              return
-            }
-            prom = prom.then(() => readFile(path).then(fileContents =>
-              importAtRule.replaceWith(fileContents)
-            ))
-          }
-        })
-        resolve(prom)
+      root.walkAtRules('nested-import', (rule) => {
+        if (!rule.params) {
+          return
+        }
+
+        const path = parseImportPath(rule.params)
+        if (!path) {
+          return
+        }
+
+        rule.replaceWith(readFile(path))
       })
     }
   }
--- site/src/main.ts
+++ site/src/main.ts
@@ -1,12 +1,4 @@
 import Vue from 'vue'
-import 'prismjs'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-markup-templating'
-import 'github-markdown-css/github-markdown.css'
-
 import App from './App.vue'
 import './css/main.css'
 
